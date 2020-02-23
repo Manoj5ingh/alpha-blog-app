87b4144c7cc49446b33ed29675c60f889a201749
Moner <themanojsingh1997@gmail.com>
2020-02-24 01:36:42 +0530

    optimized readability

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:edit, :update, :destroy, :show]
   def index
     @articles = Article.all
   end
@@ -15,15 +16,12 @@ class ArticlesController < ApplicationController
     end
   end
   def show
-    @article = Article.find params[:id]
   end
 
   def edit
-    @article = Article.find params[:id]
   end
 
   def update
-    @article = Article.find params[:id]
     if @article.update article_params
         flash[:notice] = "Article was successfully updated"
         redirect_to article_path(@article)
@@ -33,16 +31,18 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find params[:id]
     if @article.destroy
       flash[:notice] = "Article was successfully deleted"
-      redirect_to article_path(@article)
+      redirect_to articles_path
     else
       flash[:notice] = "Some error occured during deletion"
-      redirect_to article_path(@article)
+      redirect_to articles_path
     end
   end
   private def article_params
     params.require(:article).permit(:title, :desc)
+  end
+  private def set_article
+    @article = Article.find params[:id]
   end
 end 
