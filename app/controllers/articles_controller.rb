4c9724c02f964496bf89635f0fad8c7ec5304a45
Moner <themanojsingh1997@gmail.com>
2020-02-24 00:06:54 +0530

    create UI for new Article and db persist

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,13 @@
+class ArticlesController < ApplicationController
+  def new
+    @articles = Article.new 
+  end
+  def create
+    @article = Article.new article_params
+    @article.save
+    redirect_to articles_show(@article)
+  end
+  private def article_params
+    params.require(:article).permit(:title, :desc)
+  end
+end 
